73fe69f6168f7c07618eba40fee9412ca78da9bf
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-05-17 02:51:04 +0900

    Initial commit

---
+++ lib/rspec/formatter/slack.rb
@@ -0,0 +1,9 @@
+require "rspec/formatter/slack/version"
+
+module Rspec
+  module Formatter
+    module Slack
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rspec/formatter/slack/version.rb
@@ -0,0 +1,7 @@
+module Rspec
+  module Formatter
+    module Slack
+      VERSION = "0.1.0"
+    end
+  end
+end
